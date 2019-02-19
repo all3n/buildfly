f9725cc9bafefad14def576de64bd1efde4c1a21
all3nwhc <wanghch8398@163.com>
2019-02-19 14:46:57 +0800

    add build g++

--- buildfly/actions/build_action.py
+++ buildfly/actions/build_action.py
@@ -11,6 +11,7 @@
 """
 import sys
 import os
+import glob
 from buildfly.actions.basic_action import basic_action
 from buildfly.utils.yaml_conf_utils import yaml_conf_loader
 CONF_NAME="buildfly.yaml"
@@ -26,5 +27,145 @@ class build_action(basic_action):
             sys.exit(-1)
 
         app_conf = yaml_conf_loader(conf_file)
+
+        self.build_dir = app_conf.args.get('build_dir', 'build')
+        if not os.path.exists(self.build_dir):
+            os.makedirs(self.build_dir)
+        self.bins = app_conf.bins
+        self.libs = app_conf.libs
         print(app_conf)
+
+
+        self.build_flag = {}
+
+        for name, build_info in self.bins.items():
+            self.build_bin(name, build_info)
+
+        for name, build_info in self.libs.items():
+            if name not in self.build_flag:
+                self.build_library(name, build_info)
+
+    def build_dep(self, name, build_info):
+        if 'deps' not in build_info:
+            return
+        print(name, build_info)
+        deps = build_info['deps']
+        for dep in deps:
+            if dep not in self.build_flag:
+                if self.build_library(dep, self.libs[dep]):
+                    self.build_flag[dep] = True
+                else:
+                    raise Exception("build dep library:%s fail" % (dep))
+
+    def expand_pattern(self, pattern):
+        return glob.glob(pattern, recursive = True)
+
+
+    def build_bin(self, name, build_info):
+        bin_dir = os.path.join(self.build_dir, "build-bin-%s" % name)
+        if not os.path.exists(bin_dir):
+            os.makedirs(bin_dir)
+        self.build_dep(name, build_info)
+        cmds = []
+        srcs = build_info['srcs']
+        includes = build_info['includes'] if 'includes' in build_info else []
+        target = name
+        library_path = []
+        link_library = []
+        static_libs = []
+        if "deps" in build_info:
+            deps = build_info['deps']
+            for dep in deps:
+                lib_build_dir = os.path.join(self.build_dir, "build-lib-%s" % dep)
+                dep_lib_info = self.libs[dep]
+                lib_include_dir = dep_lib_info['includes']
+                lib_type = dep_lib_info.get('lib_type', 'shared')
+                includes += lib_include_dir
+                if lib_type == 'shared':
+                    library_path.append(lib_build_dir)
+                    link_library.append(dep)
+                else:
+                    static_libs.append(os.path.join(lib_build_dir, "lib%s.a" % (dep)))
+
+        include_options = " ".join(["-I%s" % i for i in includes])
+        library_path_options = " ".join(["-L%s" % i for i in library_path])
+        link_lib_options = " ".join(["-l%s" % i for i in link_library])
+        static_lib_option=" ".join(static_libs)
+
+        srcs_files = []
+        for src in srcs:
+            srcs_files += self.expand_pattern(src)
+        srcs_options = " ".join(srcs_files)
+        cflags_options = build_info.get('cflags', '')
+
+        cmds.append("g++ {cflags} {library_path_options} {link_lib_options} {include_options} -o {build_dir}/{target} {srcs} {static_lib_option}".format(
+            target = target,
+            srcs = srcs_options,
+            library_path_options = library_path_options,
+            include_options = include_options,
+            build_dir = bin_dir,
+            cflags = cflags_options,
+            link_lib_options=link_lib_options,
+            static_lib_option=static_lib_option
+        ))
+
+        for cmd in cmds:
+            print(cmd)
+            os.system(cmd)
+        return True
+
+    def build_library(self, name, build_info):
+        lib_dir = os.path.join(self.build_dir, "build-lib-%s" % name)
+        if not os.path.exists(lib_dir):
+            os.makedirs(lib_dir)
+        self.build_dep(name, build_info)
+
+        cmds = []
+        srcs = build_info['srcs']
+        includes = build_info['includes'] if 'includes' in build_info else []
+        target = name
+        include_options = " ".join(["-I%s" % i for i in includes])
+
+        srcs_files = []
+        for src in srcs:
+            srcs_files += self.expand_pattern(src)
+        srcs_options = " ".join(srcs_files)
+        cflags_options = build_info.get('cflags', '')
+        src_names = [i.rsplit(".",1)[0] for i in srcs_files]
+
+        for sn in src_names:
+            object_file = "%s/%s.o" % (lib_dir, sn)
+            object_file_dir = os.path.dirname(object_file)
+            if not os.path.exists(object_file_dir):
+                os.makedirs(object_file_dir)
+            cmds.append("g++ -c {cflags} {include_options} -o {object_file} {srcs}".format(
+                object_file=object_file,
+                srcs = srcs_options,
+                include_options = include_options,
+                cflags = cflags_options
+            ))
+        lib_type = build_info.get('lib_type', 'shared')
+        lib_objects_option = "".join(["%s/%s.o" % (lib_dir,i) for i in src_names])
+        if lib_type == 'shared':
+            target_ext='so'
+            cmds.append("g++ -shared -fPIC -o {build_dir}/lib{target}.{target_ext} {lib_objects_option}".format(
+                    target=target,
+                    lib_objects_option = lib_objects_option,
+                    target_ext=target_ext,
+                    build_dir=lib_dir
+                ))
+        else:
+            target_ext='a'
+            cmds.append("ar crv {build_dir}/lib{target}.{target_ext} {lib_objects_option}".format(
+                    target=target,
+                    lib_objects_option = lib_objects_option,
+                    target_ext=target_ext,
+                    build_dir=lib_dir
+                ))
+
+        for cmd in cmds:
+            print(cmd)
+            os.system(cmd)
+
+        return True
 
--- buildfly/utils/yaml_conf_utils.py
+++ buildfly/utils/yaml_conf_utils.py
@@ -10,6 +10,24 @@
 
 """
 import yaml
+from collections import namedtuple
+BuildConf = namedtuple('BuildConf', ['bins','libs', 'args'])
+
+
 def yaml_conf_loader(yaml_file):
     with open(yaml_file, "r", encoding='utf-8') as yf:
-        return yaml.load(yf.read())
+        conf =  yaml.load(yf.read())
+        bins = {}
+        libs = {}
+        args = {}
+        for k,v in conf.items():
+            if type(v) == dict and 'type' in v:
+                if v['type'] == 'bin':
+                    bins[k] = v
+                elif v['type'] == 'lib':
+                    libs[k] = v
+            else:
+                args[k] = v
+
+        build_conf = BuildConf(bins = bins, libs = libs, args = args)
+        return build_conf
